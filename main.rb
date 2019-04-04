242223d82b2c2a26f0fa9b99487891c8d542a538
Jamie Holdstock <jam.jar69@gmail.com>
2019-04-04 18:16:22 +0100

    Move polo sleep.

--- main.rb
+++ main.rb
@@ -26,8 +26,7 @@ def download_chart start_date, end_date, currency_pair
 	request = Net::HTTP::Get.new(uri.request_uri)
 	response = http.request(request)
 
-	sleep 0.2 # Polo doesn't like >6 requests per second
-
+	
 	# TODO: Trusting polo to return sorted data. Should sort here to be safe
 	JSON.parse(response.body)
 end	
@@ -35,44 +34,45 @@ end
 def get_month_avg mon, year
 	start_date = DateTime.new(year, mon, 1)
 	end_date = start_date.next_month
-
+	
 	btc = download_chart(start_date, end_date, "BTC_DCR")
 	usd = download_chart(start_date, end_date, "USDT_BTC")
-
+	
 	dcr_usd = []
-
+	
 	[btc.length, usd.length].min.times do |i|
 		btc_avg = [btc[i]["high"], btc[i]["low"]].avg
 		usd_avg = [usd[i]["high"], usd[i]["low"]].avg
-
+		
 		dcr_usd.push(btc_avg * usd_avg)
 	end
-
+	
 	month_avg = sprintf('%.4f', dcr_usd.avg)
 	puts "#{month_avg} USD/DCR (#{mon}-#{year})"
+	sleep 0.3 # Polo doesn't like >6 requests per second
 end
 
-# get_month_avg(3, 2017)
-# get_month_avg(4, 2017)
-# get_month_avg(5, 2017)
-# get_month_avg(6, 2017)
-# get_month_avg(7, 2017)
-# get_month_avg(8, 2017)
-# get_month_avg(9, 2017)
-# get_month_avg(10, 2017)
-# get_month_avg(11, 2017)
-# get_month_avg(12, 2017)
-# get_month_avg(1, 2018)
-# get_month_avg(2, 2018)
-# get_month_avg(3, 2018)
-# get_month_avg(4, 2018)
-# get_month_avg(5, 2018)
-# get_month_avg(6, 2018)
-# get_month_avg(7, 2018)
-# get_month_avg(8, 2018)
-# get_month_avg(9, 2018)
-# get_month_avg(10, 2018)
-# get_month_avg(11, 2018)
-# get_month_avg(12, 2018)
-# get_month_avg(1, 2019)
+get_month_avg(3, 2017)
+get_month_avg(4, 2017)
+get_month_avg(5, 2017)
+get_month_avg(6, 2017)
+get_month_avg(7, 2017)
+get_month_avg(8, 2017)
+get_month_avg(9, 2017)
+get_month_avg(10, 2017)
+get_month_avg(11, 2017)
+get_month_avg(12, 2017)
+get_month_avg(1, 2018)
+get_month_avg(2, 2018)
+get_month_avg(3, 2018)
+get_month_avg(4, 2018)
+get_month_avg(5, 2018)
+get_month_avg(6, 2018)
+get_month_avg(7, 2018)
+get_month_avg(8, 2018)
+get_month_avg(9, 2018)
+get_month_avg(10, 2018)
+get_month_avg(11, 2018)
+get_month_avg(12, 2018)
+get_month_avg(1, 2019)
 get_month_avg(2, 2019)
