d208b952b9eff576a14904fe1989c403421e0e88
Jamie Holdstock <jam.jar69@gmail.com>
2019-04-08 00:28:36 +0100

    homepage: make years collapsible. add tooltip.

--- server/homepage.go
+++ server/homepage.go
@@ -3,11 +3,14 @@ package server
 import (
 	"html/template"
 	"net/http"
+	"time"
 )
 
 type homepageContext struct {
-	FullHistory PriceHistory
-	MonthNames  []string
+	FullHistory  PriceHistory
+	MonthNames   []string
+	CurrentYear  int
+	CurrentMonth int
 }
 
 var monthNames = []string{
@@ -29,7 +32,9 @@ var monthNames = []string{
 func homePage(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("server/views/index.html")
 	t.Execute(w, homepageContext{
-		FullHistory: FullHistory,
-		MonthNames:  monthNames,
+		FullHistory:  FullHistory,
+		MonthNames:   monthNames,
+		CurrentYear:  time.Now().Year(),
+		CurrentMonth: int(time.Now().Month()),
 	})
 }
